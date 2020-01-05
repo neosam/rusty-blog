fd38780b2ed1ced83ee099ba648c2dacbb3e09ff
Simon Goller <neosam+github@posteo.de>
2020-01-05 10:08:32 +0100

    Use default values for configuration
    All values in config.yml are not optional.

--- src/config.rs
+++ src/config.rs
@@ -3,12 +3,42 @@ use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::sync::RwLock;
 
+fn default_hostname() -> String {
+    "127.0.0.1".to_string()
+}
+
+fn default_port() -> u32 {
+    8080
+}
+
+fn default_doc_path() -> String {
+    "./".to_string()
+}
+
+fn default_context() -> String {
+    String::new()
+}
+
+fn default_caching() -> bool {
+    true
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct Config {
+    #[serde(default = "default_hostname")]
     hostname: String,
+
+    #[serde(default = "default_port")]
     port: u32,
+
+    #[serde(default = "default_doc_path")]
     doc_path: String,
+
+    #[serde(default = "default_context")]
     context: String,
+
+    #[serde(default = "default_caching")]
+    caching: bool,
 }
 impl Default for Config {
     fn default() -> Config {
@@ -17,6 +47,7 @@ impl Default for Config {
             port: 8080,
             doc_path: "./".to_string(),
             context: "".to_string(),
+            caching: true,
         }
     }
 }
