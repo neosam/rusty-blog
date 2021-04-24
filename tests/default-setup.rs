b448d9faeaa7e3c533e96890f8b76d14185af700
Simon Goller <neosam+github@posteo.de>
2021-04-24 14:45:57 +0200

    Test binary data and static sub-directories

--- tests/default-setup.rs
+++ tests/default-setup.rs
@@ -30,12 +30,31 @@ fn assert_eq<T: PartialEq + Debug>(msg: impl ToString, expected: T, actual: T) -
         }).context("assert_eq failed")
     }
 }
+fn binary_assert_eq<T: PartialEq + Debug>(msg: impl ToString, expected: T, actual: T) -> anyhow::Result<()> {
+    if expected == actual {
+        Ok(())
+    } else {
+        Err(TestError{
+            msg: msg.to_string(),
+            expected: "Binary data".to_string(), 
+            actual: "Binary data".to_string(),
+        }).context("assert_eq failed")
+    }
+}
 
 fn dump_response(name: &str, response_text: &str) -> anyhow::Result<()> {
     let filename = format!("test-responses/response-{}.txt", name);
     fs::create_dir_all("test-responses")?;
     let mut file = File::create(&filename)?;
     file.write(response_text.as_bytes())?;
+    file.flush()?;
+    Ok(())
+}
+fn dump_binary_response(name: &str, response_text: &[u8]) -> anyhow::Result<()> {
+    let filename = format!("test-responses/response-{}", name);
+    fs::create_dir_all("test-responses")?;
+    let mut file = File::create(&filename)?;
+    file.write(response_text)?;
     file.flush()?;
     Ok(())
 }
@@ -57,6 +76,27 @@ fn test_response(name: &str, mime_type: &str, url: &str) -> anyhow::Result<()> {
         .read_to_string(&mut expected)?;
     
     assert_eq(&format!("{}: Expect request body to match template", name), &expected, &response_text)?;
+
+    Ok(())
+}
+fn test_binary_response(name: &str, mime_type: &str, url: &str) -> anyhow::Result<()> {
+    let response = reqwest::blocking::get(url)?;
+    assert_eq(&format!("{}: Expect status code to be 200 (successful)", name), "200", response.status().as_str())?;
+    let content_type_header = response.headers().get("content-type")
+        .with_context(|| format!("{}: No content-type header found", name))?
+        .to_str().with_context(|| format!("{}: Cannot convert content-type header to str", name))?;
+    assert_eq(&format!("{}: Expect correct mime-type", name), mime_type, content_type_header)?;
+    let mut response_text= response.bytes()?;
+    let response_body: Vec<u8> = response_text.iter().map(|x| x.clone()).collect();
+    dump_binary_response(name, &response_text)?;
+    
+    let expected_filename = format!("expected-responses/response-{}", name);
+    let mut expected = Vec::new();
+    File::open(&expected_filename)
+        .with_context(|| format!("Could not open response file: {}", expected_filename))?
+        .read_to_end(&mut expected)?;
+    
+    binary_assert_eq(&format!("{}: Expect request body to match template", name), &expected, &response_body)?;
 
     Ok(())
 }
@@ -99,6 +139,7 @@ fn default_setup_test() {
         test_response("simple-post", "text/html", "http://localhost:2536/post/2019-11-26-simple-post.html")?;
         test_response("first-post", "text/html",  "http://localhost:2536/post/2019-11-24-first-post.html")?;
         test_response("static-text", "text/plain",  "http://localhost:2536/static/test.txt")?;
+        test_binary_response("image", "image/jpeg", "http://localhost:2536/static/images/neosam.jpeg")?;
     
         Ok(())
     }() {
